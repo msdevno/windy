dd5e71dc8eee009302397a26f844c26b1c2a69d0
pedias <pedias@microsoft.com>
2015-10-01 08:26:00 +0200

    Added WindmillData

---
+++ Windy.Domain/WindmillData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Windy.Domain
+{
+    [DataContract(Name ="windmilldata")]
+    public class WindmillData
+    {
+        [DataMember(Name ="client", Order = 1)]
+        public string Client { get; set; }
+
+        [DataMember(Name = "millId", Order = 2)]
+        public string MillId { get; set; }
+
+        [DataMember(Name = "megawatt", Order = 3)]
+        public double MegaWatt { get; set; }       
+    }
+}
