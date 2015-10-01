8b92ac10596b04dd5450c183aa3c822e1c5dd9b8
pedias <pedias@microsoft.com>
2015-10-01 06:25:26 +0200

    Initial Commit

---
+++ Windy/Entitities/WindmillData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windy.Entitities
+{
+    [Serializable]
+    public class WindmillData
+    {
+        public string Client { get; set; }
+
+        public string MillId { get; set; }
+
+        public double MegaWatt { get; set; }
+
+        public static  byte[] AsByteArray(WindmillData data)
+        {
+            if (data == null)
+                return null;
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, data);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ Windy/Program.cs
@@ -0,0 +1,40 @@
+﻿using FizzWare.NBuilder;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Linq;
+using Windy.Entitities;
+
+namespace Windy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomGenerator = new RandomGenerator((int)DateTime.Now.Ticks);
+            var eventHubClient = EventHubClient.CreateFromConnectionString("Endpoint=sb://windypedro.servicebus.windows.net/;SharedAccessKeyName=PedrosTransmissionPolicy;SharedAccessKey=lAhSvgXwQFDGKqfMTcs9ptoMwwP2XnCAAbW5cjnBfP0=", "windmillseventhub");
+
+            while (true)
+            {
+                // Generate tons of Random data
+                var newWindmillSamples = Builder<WindmillData>
+                    .CreateListOfSize(32)
+                    .All()
+                        .With(o => o.MillId = randomGenerator.Next(1, 800).ToString())
+                        .With(o => o.Client = "Bærum")
+                        .With(o => o.MegaWatt = randomGenerator.Next(0.01, 3.0))
+                    .Build().ToList();
+
+
+                // post random data to Event Hub
+                var byteArrays = newWindmillSamples.Select(t => new EventData(WindmillData.AsByteArray(t)));
+                eventHubClient.SendBatch(byteArrays);
+
+                var first = newWindmillSamples.First();
+
+                Console.WriteLine($"Just sent data for Client: {first.Client}, Mill: {first.MillId}, Producing {first.MegaWatt} MW");
+            }
+        
+
+        }
+    }
+}
