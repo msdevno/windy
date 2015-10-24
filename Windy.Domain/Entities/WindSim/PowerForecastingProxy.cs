45fb22d89d66495144d9dd62acf92982747e9c48
ruisantos <rsantos.mails@gmail.com>
2015-10-24 20:48:11 +0200

    Add integraion with WindSim Power Forecasting API

---
+++ Windy.Domain/Entities/WindSim/PowerForecastingProxy.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windy.Domain.Entities.WindSim
+{
+    public class PowerForecastingProxy
+    {
+        /// <summary>
+        /// Retrive Power Forecast Data fro WindSim API
+        /// </summary>
+        /// <param name="key">WindSim WindFarm Key</param>
+        /// <returns>Power Forecast data for a specific WindFarm</returns>
+        public static PowerForecastData GetWindFarmData(string key)
+        {
+
+            string requestUrl = $"https://windsimapi.azurewebsites.net/api/V1/Stream/{key}";
+            using (var client = new WebClient())
+            {
+                ///TODO: Check Web Errors
+                return ParseForecastingData(client.DownloadString(new Uri(requestUrl)));
+            }
+        }
+
+        /// <summary>
+        /// Parse an WindSim Power Forecast result into TimeSeries
+        /// </summary>
+        /// <param name="data"> String all the data </param>
+        /// <returns>TimeSeries of Power Forecast</returns>
+        private static PowerForecastData ParseForecastingData(string data)
+        {
+            var result = new PowerForecastData();
+            var tmpColumnIndex = new Dictionary<int, string>();
+            foreach (var line in data.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (line.StartsWith("Date/Time	ALL"))//FirstLine
+                {
+                    int column = 1;
+                    Array.ForEach(line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray(), t =>
+                    {
+                        result.PowerForecast.Add(t.Trim(), new Dictionary<DateTime, ForecastElementInfo>());
+                        tmpColumnIndex.Add(column++, t.Trim());
+                    });
+                    continue;
+                }
+                var powerLineValues = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                DateTime date = DateTime.ParseExact(powerLineValues[0], "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                for (int i = 1; i < powerLineValues.Length; ++i)
+                {
+                    result.PowerForecast[tmpColumnIndex[i]].Add(date, new ForecastElementInfo()
+                    {
+                        Name = tmpColumnIndex[i],
+                        PowerKW = double.Parse(powerLineValues[i], new CultureInfo("en-US"))
+                    });
+                }
+            }
+            return result;
+        }
+
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class PowerForecastData
+        {
+            private Dictionary<string, Dictionary<DateTime, ForecastElementInfo>> _powerForecast = new Dictionary<string, Dictionary<DateTime, ForecastElementInfo>>();
+            public Dictionary<string, Dictionary<DateTime, ForecastElementInfo>> PowerForecast
+            {
+                get { return _powerForecast; }
+                set { _powerForecast = value; }
+            }
+        }
+
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class ForecastElementInfo
+        {
+            public ForecastElementType Type { get; private set; }
+
+            private string _name;
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+                set
+                {
+                    _name = value;
+                    Type = value == "ALL" ? ForecastElementType.WindFarm : ForecastElementType.Turbine;
+                }
+            }
+
+            public double PowerKW { get; set; }
+        }
+
+
+        /// <summary>
+        /// Represents the ElementType of Forecast
+        /// </summary>
+        public enum ForecastElementType
+        {
+            WindFarm = 0,
+            Turbine
+        }
+    }
+}
---
+++ Windy.Tests/PowerForecastProxyTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windy.Domain.Entities.WindSim;
+using System.Linq;
+
+namespace Windy.Tests
+{
+    [TestClass]
+    public class PowerForecastProxyTest
+    {
+        [TestMethod]
+        public void RetriveData()
+        {
+
+            var powerForecastingData=PowerForecastingProxy.GetWindFarmData("62c3ecac-5131-421e-b24d-4419ccd59264/22a0edef-ef56-4f10-960c-8d6540ef07a7");
+
+            Assert.IsNotNull(powerForecastingData,"The result is Null");
+            Assert.IsNotNull(powerForecastingData.PowerForecast, "The parsed values is Null");
+            Assert.AreEqual(powerForecastingData.PowerForecast.Keys.Count, 45);
+
+            var totalWindFarmPowerForecastTimeSeries = powerForecastingData.PowerForecast.FirstOrDefault(x => x.Value.Any(c => c.Value.Type == PowerForecastingProxy.ForecastElementType.WindFarm)).Value;
+
+            var totalFirstTuebinePowerForecastTimeSeries = powerForecastingData.PowerForecast.FirstOrDefault(x => x.Value.Any(c => c.Value.Type == PowerForecastingProxy.ForecastElementType.Turbine)).Value;
+
+            Assert.AreEqual(totalWindFarmPowerForecastTimeSeries.Count(), totalFirstTuebinePowerForecastTimeSeries.Count, "The time series of WindFarm should be equal to First Turbine ");
+        }
+    }
+}
