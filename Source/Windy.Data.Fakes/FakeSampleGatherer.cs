8bc187c71ac7aa8eec19bf4473845abb46231ddc
Pedro G. Dias <pedro@digitaldias.com>
2015-12-04 17:58:15 +0100

    Tidied up FakeSampleGatherer

--- Source/Windy.Data.Fakes/FakeSampleGatherer.cs
+++ Source/Windy.Data.Fakes/FakeSampleGatherer.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windy.Domain.Contracts;
 using Windy.Domain.Contracts.Calculators;
 using Windy.Domain.Contracts.Queries;
 using Windy.Domain.Contracts.Yr;
 using Windy.Domain.Entities;
 using Windy.Domain.Entities.Samples;
+using Windy.Domain.Entities.Yr;
 
 namespace Windy.Data.Fakes
 {
@@ -33,19 +32,29 @@ namespace Windy.Data.Fakes
             {
                 foreach (var mill in farm.Windmills)
                 {
-                    var weatherData = _weatherProxy.GetWeatherDataForLocation(mill.Location.Latitude, mill.Location.Longitude);
-                    var locationData = weatherData.product.time[0].location;
+                    var locationData = GetLocalWeatherForMill(mill);
 
+                    // Sample Windspeed
                     var windSpeed = (double)locationData.windSpeed.mps;
                     samples.Add(new WindSpeedSample { WindFarmId = farm.Id, WindmillId = mill.Id, SampleTime = DateTime.Now, WindSpeed = windSpeed });
+
+                    // Sample Temperature
                     samples.Add(new TemperatureSample { WindFarmId = farm.Id, WindmillId = mill.Id, SampleTime = DateTime.Now, Temperature = (double)locationData.temperature.value });
 
+                    // Sample Megawatt
                     var megawatt = _megawattCalculator.CalculateForGeneratorBasedOnWindSpeed(mill.Generator, windSpeed);
                     samples.Add(new MegawattSample { WindFarmId = farm.Id, WindmillId = mill.Id, SampleTime = DateTime.Now, MegaWatt = megawatt });
                 }
                 _logger.LogInformation($"Wind Farm: '{farm.Name}' {farm.Windmills.Count()} windmill samples gathered.");
             }
             return samples;
+        }
+
+        private weatherdataProductTimeLocation GetLocalWeatherForMill(Windmill mill)
+        {
+            var weatherData = _weatherProxy.GetWeatherDataForLocation(mill.Location.Latitude, mill.Location.Longitude);
+            var locationData = weatherData.product.time[0].location;
+            return locationData;
         }
     }
 }
